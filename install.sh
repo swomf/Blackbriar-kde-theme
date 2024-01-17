015dd06e9b3a4db5478d3840c10a62e916bd8198
swomf <swomf@proton.me>
2024-01-16 20:52:44 -0500

    Reduce code in install.sh, deprecate uninstall.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SRC_DIR=$(cd $(dirname $0) && pwd)
-
 ROOT_UID=0
+
+THEME_NAME=Blackbriar
+SRC_DIR=$(cd $(dirname $0) && pwd)
 
 # Destination directory
 if [ "$UID" -eq "$ROOT_UID" ]; then
@@ -21,52 +22,21 @@ else
   WALLPAPER_DIR="$HOME/.local/share/wallpapers"
 fi
 
-THEME_NAME=Blackbriar
-
 # COLORS
-CDEF=" \033[0m"                                     # default color
-CCIN=" \033[0;36m"                                  # info color
-CGSC=" \033[0;32m"                                  # success color
-CRER=" \033[0;31m"                                  # error color
-CWAR=" \033[0;33m"                                  # warning color
-b_CDEF=" \033[1;37m"                                # bold default color
-b_CCIN=" \033[1;36m"                                # bold info color
-b_CGSC=" \033[1;32m"                                # bold success color
-b_CRER=" \033[1;31m"                                # bold error color
-b_CWAR=" \033[1;33m"                                # bold warning color
+CDEF="\033[0m"                                     # default color
+CCIN="\033[0;36m"                                  # info color
+CGSC="\033[0;32m"                                  # success color
+CRER="\033[0;31m"                                  # error color
+CWAR="\033[0;33m"                                  # warning color
+b_CDEF="\033[1;37m"                                # bold default color
+b_CCIN="\033[1;36m"                                # bold info color
+b_CGSC="\033[1;32m"                                # bold success color
+b_CRER="\033[1;31m"                                # bold error color
+b_CWAR="\033[1;33m"                                # bold warning color
 
-# Echo like ... with flag type and display message colors
-prompt () {
-  case ${1} in
-    "-s"|"--success")
-      echo -e "${b_CGSC}${@/-s/}${CDEF}";;          # print success message
-    "-e"|"--error")
-      echo -e "${b_CRER}${@/-e/}${CDEF}";;          # print error message
-    "-w"|"--warning")
-      echo -e "${b_CWAR}${@/-w/}${CDEF}";;          # print warning message
-    "-i"|"--info")
-      echo -e "${b_CCIN}${@/-i/}${CDEF}";;          # print info message
-    *)
-    echo -e "$@"
-    ;;
-  esac
-}
+installer() {
+  local action=$1 # install or uninstall
 
-usage() {
-  cat << EOF
-Usage: $0 [OPTION]...
-
-OPTIONS:
-  -n, --name NAME         Specify theme name (Default: $THEME_NAME)
-  -t, --theme VARIANT     Specify theme color variant(s) [default|nord] (Default: All variants)s)
-  -c, --color VARIANT     Specify color variant(s) [standard|light|dark] (Default: All variants)s)
-  --rimless VARIANT       Specify rimless variant
-
-  -h, --help              Show help
-EOF
-}
-
-install() {
   local name=$THEME_NAME
 
   local AURORAE_THEME="${AURORAE_DIR}/${name}"
@@ -91,18 +61,68 @@ install() {
   [[ -d ${WALLPAPER_THEME} ]] && rm -rf ${WALLPAPER_THEME}
   [[ -d ${WALLPAPER_DIR}/${name} ]] && rm -rf ${WALLPAPER_DIR}/${name}
 
-  cp -r ${SRC_DIR}/aurorae/${name}                                      ${AURORAE_DIR}
+  if [[ $action == "uninstall" ]]; then
+    echo -e "${b_CGSC}Uninstalled.${CDEF}"
+    exit 0
+  fi
 
-  cp -r ${SRC_DIR}/color-schemes/${name}.colors                    ${SCHEMES_DIR}
-  cp -r ${SRC_DIR}/Kvantum/${name}                                              ${KVANTUM_DIR}
+  cp -r ${SRC_DIR}/aurorae/${name}                                                           ${AURORAE_DIR}
+
+  cp -r ${SRC_DIR}/color-schemes/${name}.colors                                              ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/Kvantum/${name}                                                           ${KVANTUM_DIR}
 
   cp -r ${SRC_DIR}/plasma/desktoptheme/${name}                                               ${PLASMA_DIR}
   cp -r ${SRC_DIR}/plasma/desktoptheme/${name}                                               ${PLASMA_THEME}
 
-  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}/*                             ${PLASMA_THEME}
-  cp -r ${SRC_DIR}/color-schemes/${name}.colors                    ${PLASMA_THEME}/colors
-  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.swomf.${name}                       ${LOOKFEEL_DIR}
-  cp -r ${SRC_DIR}/wallpaper/${name}                                                 ${WALLPAPER_DIR}
+  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}/*                                             ${PLASMA_THEME}
+  cp -r ${SRC_DIR}/color-schemes/${name}.colors                                              ${PLASMA_THEME}/colors
+  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.swomf.${name}                             ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/wallpaper/${name}                                                         ${WALLPAPER_DIR}
+
+  echo -e "${b_CGSC}Installed.${CDEF}"
 }
 
-install
+usage() {
+  cat << EOF
+Usage: ./install.sh [OPTION]
+Install Blackbriar KDE into the current user HOME directory.
+If no argument is specified, install the theme.
+
+  -i, --install         Install theme (default action)
+  -u, --uninstall       Uninstall theme
+  -h, --help            Print this help message
+EOF
+}
+
+main() {
+  local action=
+  case "$1" in
+    ""|"-i"|"--install")
+      echo -e "${b_CGSC}Installing${CDEF} theme in ${CCIN}${HOME}${CDEF}."
+      action="install"
+      ;;
+    "-u"|"--uninstall")
+      echo -e "${b_CRER}Uninstalling${CDEF} theme in ${CCIN}${HOME}${CDEF}."
+      action="uninstall"
+      ;;
+    "-h"|"--help")
+      usage
+      exit 0
+      ;;
+    *)
+      echo -e "${CRER}Error: Invalid option.${CDEF}"
+      exit 1
+      ;;
+  esac
+
+  read -n 1 -p "Continue? [y/N] " input
+  echo
+  if [ "$input" != "y" ] && [ "$input" != "Y" ]; then
+    echo -e "${CRER}Error: Aborted by user.${CDEF}"
+    exit 1
+  fi
+
+  installer $action
+}
+
+main $1
